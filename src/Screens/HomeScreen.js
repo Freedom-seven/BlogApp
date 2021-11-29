ea01122f0eb98fa24e22a8b4be664546f0917243
FREEDOM <freewhynane62@gmail.com>
2021-11-29 16:43:23 +0000

    add home and detail screens

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import BottomTab from '../BottomTab';
+
+const HomeScreen = () => {
+    return <BottomTab/>
+    
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
